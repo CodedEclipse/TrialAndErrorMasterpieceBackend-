5c27d009ca9bbc2503ac4183cb154d55d6a56b2c
Manish Deotale <manishdeotalemr@gmail.com>
2024-12-11 19:04:06 +0530

    chnage

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const adminRoutes = express.Router();
 const AdminController = require('../controllers/admin.controller');
 const { decryptRequestData,encryptResponseData } = require('../middlewares/EncryptDecrypt');
-
+const jwtAdmin = require('../middlewares/AdminJwtToken')
 
 adminRoutes.use(decryptRequestData);
 adminRoutes.use(encryptResponseData);
 
 adminRoutes.get('/test', AdminController.testApi);
 adminRoutes.post('/adminLogin', AdminController.adminLogin);
-adminRoutes.post('/getHashPassword', AdminController.getHashPassword);
+adminRoutes.post('/getHashPassword',jwtAdmin, AdminController.getHashPassword);
+adminRoutes.post('/logout',jwtAdmin, AdminController.logout);
 
 module.exports = adminRoutes;
